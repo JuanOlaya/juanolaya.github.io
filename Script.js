2a8ad0b6bfa83a37b848f4cd0459409567c4152a
Juan Olaya <juanolayafi@gmail.com>
2019-05-09 21:11:14 -0500

    Quitaron comentarios

--- Script.js
+++ Script.js
@@ -6,11 +6,11 @@ function setup() {
 	canvas=createCanvas(windowWidth, windowHeight);
 	canvas.position(0,0);
 	canvas.style('z-index',-1);
-  
+
 	background(50);
 
   for (var i=0; i<10; i++) {
-    pointList.push(new Point(random(width-20), random(height-20), random(-0.01, 0.01), 
+    pointList.push(new Point(random(width-20), random(height-20), random(-0.01, 0.01),
       						random(-0.01, 0.01), random(-0.01, 0.01), random(-0.01, 0.01) ));
   }
 }
@@ -21,16 +21,15 @@ function draw() {
   for (var i=0; i<pointList.length; i++) {
     pointList[i].mover();
     noStroke();
-    fill( 253, 106, 2, i*11+30);  
+    fill( 253, 106, 2, i*11+30);
     triangle(pointList[i].location.x, pointList[i].location.y, width/4, height/8, width - width/4, height/8-25);
-  	//print(i*8+30);
 	}
 	texto();
 }
 
 function texto(){
 	noStroke();
-	
+
 	fill(254, 247, 229,190);
 	textFont('Gotham');
 	textAlign(CENTER);
@@ -40,11 +39,8 @@ function texto(){
 }
 
 		document.querySelector("#btn1").addEventListener('click',function(){
-			//location.href = 'https://www.openprocessing.org/user/65585/';
 			window.open( 'https://www.openprocessing.org/user/65585/');
 		})
 		document.querySelector("#btn2").addEventListener('click',function(){
-			//location.href = 'https://github.com/JuanOlaya/';
 			window.open('https://github.com/JuanOlaya/');
 		})
-
