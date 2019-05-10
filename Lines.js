e4779ee94131e086ec2ce38ba47e492f4bb24071
Juan Olaya <juanolayafi@gmail.com>
2019-05-09 20:12:37 -0500

    Update Lines.js

--- Lines.js
+++ Lines.js
@@ -45,37 +45,5 @@ function texto(){
 	else{
 		//fill(245,245,220,170);
 		fill(254, 247, 229,190);
-	}
-	
-	//textStyle(NORMAL);
-	textSize(20);
-	text("OpenProcessing", width/2, height - 3*(height/11) );
-	//noFill();
-	//stroke(254, 247, 229,190);
-	fill(255,170,0,45);
-	rect(width/2-200,height - 3*(height/11)-30,400,50);
-	fill(254, 247, 229,190);
-	
-	
-	text("Github Repository", width/2, height - 2*(height/11) );
-	fill(255,170,0,45);
-	rect(width/2-200,height - 2*(height/11)-30,400,50);
-	fill(254, 247, 229,190);
-	
-	text("Youtube Videos", width/2, height - (height/11));
-	fill(255,170,0,45);
-	rect(width/2-200,height - (height/11)-30,400,50);
-	fill(254, 247, 229,190);
-}
-
-function mousePressed(){
-	if(mouseX > (width/2-200) && mouseX <(width/2+200) && mouseY> (height - 3*(height/11)-30) && mouseY<(height - 3*(height/11)+20) ){
-		window.open('https://www.openprocessing.org/user/65585#sketches');
-	}
-	if(mouseX> (width/2-200) && mouseX<(width/2+200) && mouseY> (height - 2*(height/11)-30) && mouseY<(height - 2*(height/11)+20) ){
-		window.open('https://github.com/JuanOlaya');
-	}
-	if(mouseX> (width/2-200) && mouseX<(width/2+200) && mouseY> (height - (height/11)-30) && mouseY<(height - (height/11)+20) ){
-		window.open('https://www.youtube.com/playlist?list=PLBD3ein1xee3-U3jtAEAsn_oqIB82Vh5B');
-	}
+	}	
 }
