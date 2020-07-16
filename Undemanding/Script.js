75512c71fad61b037c4c95d6d6f78cacfa9364f7
Juan Olaya <juanolayafi@gmail.com>
2020-07-16 16:35:09 +0200

    Update Script.js

--- Undemanding/Script.js
+++ Undemanding/Script.js
@@ -26,8 +26,8 @@ var flatPlat=30;
 
 function setup() {
   //createCanvas(windowWidth, windowHeight);
-  createCanvas(640,480);
-  //createCanvas(1366, 768);
+  //createCanvas(640,480);
+  createCanvas(1366, 768);
   //createCanvas(1024, 768);
 	//createCanvas(1200, 550);
  
