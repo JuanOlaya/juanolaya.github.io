a3f9300c6bb380e586f6265050b8bc43e1e8502b
Juan Olaya <juanolayafi@gmail.com>
2021-08-01 00:14:04 +0200

    Fullscreen

--- WindowToAnotherWorld/scripts/script.js
+++ WindowToAnotherWorld/scripts/script.js
@@ -265,8 +265,26 @@ document.getElementById("leftArrowIcon").addEventListener("click", function() {
     updateDisplays();
 });
 
+var elem = elem = document.documentElement;
+
 document.getElementById("fullscreenIcon").addEventListener("click", function() {
-    fullscreen(!fullscreen());
+    if (!document.fullscreenElement){
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    }else{
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+          }
+    }
 });
 
 function updateDisplays(){
