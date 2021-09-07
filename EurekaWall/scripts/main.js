862f6d5a6a6c5a399ea4533fa8913e03f36c8b26
Juan Olaya <juanolayafi@gmail.com>
2021-09-07 16:34:14 +0200

    gvjh

--- EurekaWall/scripts/main.js
+++ EurekaWall/scripts/main.js
@@ -10,7 +10,36 @@ function draw() {
     background(" #3A3A3A  ");
     fill("#D7B42A");
     noStroke();
-    rect(0,0,width/4,height);
+    rect(0,0,width*0.29,height);
     /*ellipse(width,height,1.75*width);*/
     
   }
+
+
+
+var elem = document.documentElement;
+
+document.getElementById("fullscreenIcon").addEventListener("click", function() {
+    
+    if (!document.fullscreenElement){
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { // Safari 
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { // IE11 
+            elem.msRequestFullscreen();
+        }
+        document.getElementById("fullscreenIcon").innerHTML = `fullscreen_exit`;
+    } else{
+     if (document.fullscreenElement){
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) { // Safari 
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { // IE11 
+            document.msExitFullscreen();
+          }
+          document.getElementById("fullscreenIcon").innerHTML = `fullscreen`;
+        }
+    }
+});
