6d9720410b6483d96d67ad4edef336c0422b4fa2
Juan Olaya <juanolayafi@gmail.com>
2021-10-01 23:56:43 +0200

    dfsdf

--- EurekaWall/Scripts/Menu.js
+++ EurekaWall/Scripts/Menu.js
@@ -580,9 +580,10 @@ class PiePiece {
         stroke("#947BD3");
         strokeWeight(4);
         noFill();
-        //fill(255);
-        ellipse(480, -150, 120, 120);
         */
+        fill("#292929");
+        ellipse(480, -150, 120, 120);
+        
         //console.log("EN IND1"+border1);
       if (index == 5) {
           //console.log("EN IND1"+index);
