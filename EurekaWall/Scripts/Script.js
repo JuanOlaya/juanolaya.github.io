45d847a114521088241b77ca9336045605828ee5
Juan Olaya <juanolayafi@gmail.com>
2021-10-01 22:58:08 +0200

    gdfgf

--- EurekaWall/Scripts/Script.js
+++ EurekaWall/Scripts/Script.js
@@ -111,13 +111,13 @@ function windowResized(){
 
 function preload() {
 	
-	/*
+	
 	img1 = loadImage("assets/demo.svg");
 	img2 = loadImage("assets/museFlow.svg");
 	img3 = loadImage("assets/particleCanvas.svg");
 	img4 = loadImage("assets/balconity.svg");
 	img5 = loadImage("assets/grid.svg");
-*/
+
 
 	//photo1 = loadImage('Assets/Images/woman1.png');
 	//photo1 = loadImage("file:///C:/Users/juanf/OneDrive/Documentos/PhD/Interactive%20Wall/Assets/Images/woman1.png");
