f9c8715c775e30b80fab31d4378eb1121b32d708
Juan Olaya <juanolayafi@gmail.com>
2023-09-09 15:58:20 -0700

    hh

---
+++ HCI_BHT_Website/sketch.js
@@ -0,0 +1,56 @@
+var diameter=80;
+var status=0;
+
+function setup() {
+  createCanvas(windowWidth , windowHeight);
+}
+
+function draw() {
+  background("#D7D3D8");
+  
+  fill("#E23C12");
+  noStroke();
+  ellipse(15*width/100, 43*height/100,diameter,diameter);
+  
+  fill("#BD320F");
+  noStroke();
+  ellipse(38*width/100, 43*height/100,diameter,diameter);
+  
+  fill("#97280C");
+  noStroke();
+  ellipse(62*width/100, 43*height/100,diameter,diameter);
+  
+  fill("#711E09");
+  noStroke();
+  ellipse(85*width/100, 43*height/100,diameter,diameter);
+  
+  
+   fill("#E23C12");
+  noStroke();
+  ellipse(15*width/100, 76*height/100,diameter,diameter);
+  
+  fill("#BD320F");
+  noStroke();
+  ellipse(38*width/100, 76*height/100,diameter,diameter);
+  
+  fill("#97280C");
+  noStroke();
+  ellipse(62*width/100, 76*height/100,diameter,diameter);
+  
+
+  if(mouseIsPressed)
+  {
+    if(50>dist(mouseX,mouseY,62*width/100, 43*height/100))  
+    {
+      console.log("Entra");
+      status=-1;
+    }
+  }
+  if(status==-1)
+  {
+    if(diameter>0)
+    {
+         diameter--;
+    }
+  }  
+}
