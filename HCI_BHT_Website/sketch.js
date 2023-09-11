419142386d07b1c66e067401e3aaae034fae1603
Juan Olaya <juanolayafi@gmail.com>
2023-09-11 13:53:19 -0700

    xcyc

--- HCI_BHT_Website/sketch.js
+++ HCI_BHT_Website/sketch.js
@@ -2,16 +2,23 @@ var diameter=80;
 var statusMenu=0;
 var statusGeneral=1;
 let img;
+var canvas;
+var defaultCanvas;
 
-function setup() {
-    createCanvas(windowWidth , windowHeight);
+function setup() 
+{
+    canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0,0);	  
 	canvas.style("z-index",-1);
+    
     //defaultCanvas=document.getElementById("defaultCanvas0");
+    //defaultCanvas.style.display = "none";
+    
     //img = loadImage('assets/HCI_logo.svg');
 }
 
-function draw() {
+function draw() 
+{
     background("#1C3147");
     //image(img, 178, 45, 234*2, 29*2);
 
@@ -19,11 +26,11 @@ function draw() {
 
     fill("#E23C12");
     noStroke();
-    ellipse(20*width/100, 30*height/100,diameter,diameter);
+    ellipse(20*width/100, 25*height/100,diameter,diameter);
 
     fill("#D10B5B");  //#BD320F
     noStroke();
-    ellipse(50*width/100, 30*height/100,diameter,diameter);
+    ellipse(50*width/100, 25*height/100,diameter,diameter);
 
     /*
     fill("#711E09");
@@ -35,11 +42,11 @@ function draw() {
     //fill("#E23C12");
     fill("#189CD8");      //#97280C
     noStroke();
-    ellipse(20*width/100, 60*height/100,diameter,diameter);
+    ellipse(20*width/100, 55*height/100,diameter,diameter);
 
     fill("#BD320F");
     noStroke();
-    ellipse(50*width/100, 60*height/100,diameter,diameter);
+    ellipse(50*width/100, 55*height/100,diameter,diameter);
 
     /*
     fill("#189CD8");      //#97280C
@@ -49,22 +56,22 @@ function draw() {
 
     fill("#DE2697");  // #97280C
     noStroke();
-    ellipse(20*width/100, 90*height/100,diameter,diameter);
+    ellipse(20*width/100, 85*height/100,diameter,diameter);
 
 
     if(mouseIsPressed)
     {
-    if(50>dist(mouseX,mouseY,80*width/100, 43*height/100))  
-    {
-        console.log("Entra");
-        statusMenu=-1;
-    }
+        if(50>dist(mouseX,mouseY,80*width/100, 43*height/100))  
+        {
+            console.log("Entra");
+            statusMenu=-1;
+        }
     }
     if(statusMenu==-1)
     {
-    if(diameter>0)
-    {
+        if(diameter>0)
+        {
             diameter--;
-    }
+        }
     }  
 }
