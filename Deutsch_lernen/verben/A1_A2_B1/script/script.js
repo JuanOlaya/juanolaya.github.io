0e1f7ce13f37d8cbf0c461deb3c686e021970d86
Juan Olaya <juanolayafi@gmail.com>
2026-02-23 10:45:43 +0100

    up

--- Deutsch_lernen/verben/A1_A2_B1/script/script.js
+++ Deutsch_lernen/verben/A1_A2_B1/script/script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const levelConfig = {
         'A1_1': { groupCount: 14, displayName: 'A1.1' },
         'A1_2': { groupCount: 14, displayName: 'A1.2' },
-        'A2_1': { groupCount: 14, displayName: 'A2.1' },
+        'A2_1': { groupCount: 13, displayName: 'A2.1' },
         'A2_2': { groupCount: 17, displayName: 'A2.2' },
         'B1_1': { groupCount: 20, displayName: 'B1.1' },
         'B2_1': { groupCount: 2, displayName: 'B2.1' }
