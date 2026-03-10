9bedb19b9eddd6f0157501fea68d81a75c8c14db
Juan Olaya <juanolayafi@gmail.com>
2026-03-10 23:17:59 +0100

    up

---
+++ Deutsch_lernen/verben/A1_A2_B1/find_duplicates.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const indexFile = require('path').join(__dirname, 'json', 'verbs_index.json');
+const data = JSON.parse(fs.readFileSync(indexFile, 'utf8'));
+
+const verbMap = {};
+for (let group of data.groups) {
+    for (let verb of group.verbs) {
+        if (!verbMap[verb]) verbMap[verb] = [];
+        verbMap[verb].push(`${group.level} ${group.groupNameGerman}`);
+    }
+}
+
+for (let verb in verbMap) {
+    if (verbMap[verb].length > 1) {
+        console.log(`DUPLICATE FOUND: ${verb} in ${verbMap[verb].join(', ')}`);
+    }
+}
